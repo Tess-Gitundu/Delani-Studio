31441bb7163b3adfa43cfec8245c98845e9f3c3f
Tess <njerigitundu003@gmail.com>
2022-03-12 17:30:28 +0300

    Add jquery function to landing4

---
+++ script.js
@@ -0,0 +1,18 @@
+// backend logic
+// $(document).ready function((){
+//     $(".design").click function() {
+//             $(".des").toggle("slow", function()){
+//                 $("img").hide f
+
+//             })
+//         });
+
+  
+    // $("img").show
+
+
+
+
+
+
+// frontend logic
